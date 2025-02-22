702a938416da6ec34e177d08957e204c880ce8a6
viggotico <145546620+viggotico@users.noreply.github.com>
2025-02-22 16:18:35 +0100

    Update ProjectDescription.tsx

--- src/components/ProjectDescription.tsx
+++ src/components/ProjectDescription.tsx
@@ -12,11 +12,13 @@ interface ProjectDescriptionProps {
 export const ProjectDescription = ({ title, description, url, isEnglish }: ProjectDescriptionProps) => {
     const isMobile = useMediaQuery({ query: '(max-aspect-ratio: 3/2.7)' });
 
-    return <Column style={{ gap: isMobile ? '4vw' : '2vw' }}>
-        <div className={style.projectDescription}>
-            <p><strong>{title}</strong></p>
-            <span>{description}</span>
-        </div>
-        <a href={url} target='_blank'><button>{isEnglish ? 'see more' : 'se mere'}</button></a>
-    </Column>;
+    return <a href={url} target='_blank'>
+        <Column style={{ gap: isMobile ? '4vw' : '2vw' }}>
+            <div className={style.projectDescription}>
+                <p><strong>{title}</strong></p>
+                <span>{description}</span>
+            </div>
+            <button>{isEnglish ? 'see more' : 'se mere'}</button>
+        </Column>
+    </a>;
 }
